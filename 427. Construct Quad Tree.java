7d4c0b4dbc8c70ee259386a22c544021b1755a2a
Ankitha C <ankithac45@gmail.com>
2023-02-28 17:26:32 +0530

    Update 427. Construct Quad Tree.java

--- 427. Construct Quad Tree.java
+++ 427. Construct Quad Tree.java
@@ -39,9 +39,8 @@ class Node {
 */
 
 class Solution {
-
-    public boolean AllValues(int [][] grid,int sr,int er,int sc,int ec){
-        int val=grid[sr][sc];
+    public boolean allValues(int[][] grid,int sr,int er,int sc,int ec){
+        int val= grid[sr][sc];
         for(int i=sr;i<er;i++){
             for(int j=sc;j<ec;j++){
                 if(grid[i][j]!=val){
@@ -51,27 +50,27 @@ class Solution {
         }
         return true;
     }
-
-    public Node Solve(int [][]grid,int sr,int er,int sc,int ec){
-        if(AllValues(grid,sr,er,sc,ec)==true){
+    public Node Solve(int[][] grid,int sr,int er,int sc,int ec){
+        if(allValues(grid,sr,er,sc,ec)==true){
             boolean val;
             if(grid[sr][sc]==1){
                 val=true;
             }else{
                 val=false;
             }
-            return new Node(val,true);
-        }else{
-            Node node=new Node(false,false);
-            int half=(er-sr)/2;
-            node.topLeft=Solve(grid,sr,sr+half,sc,sc+half);
-            node.topRight=Solve(grid,sr,sr+half,sc+half,ec);
-            node.bottomLeft=Solve(grid,sr+half,er,sc,sc+half);
-            node.bottomRight=Solve(grid,sr+half,er,sc+half,ec);
-            return node;
-        }
+        return new Node(val,true);
+    }else{
+        Node node=new Node(false,false);
+        //Node node=new Node(true,false);
+        int half=(er-sr)/2;
+        node.topLeft=Solve(grid,sr,sr+half,sc,sc+half);
+        node.topRight=Solve(grid,sr,sr+half,sc+half,ec);
+        node.bottomLeft=Solve(grid,sr+half,er,sc,sc+half);
+        node.bottomRight=Solve(grid,sr+half,er,sc+half,ec);
+        return node;
     }
-    public Node construct(int[][] grid) {
-        return Solve(grid,0,grid.length,0,grid.length);
-    }
+}
+public Node construct(int[][] grid) {
+    return Solve(grid,0,grid.length,0,grid.length);
+}
 }
