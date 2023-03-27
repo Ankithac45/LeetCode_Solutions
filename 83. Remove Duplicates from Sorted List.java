be290973dda1327ba50f92bb5a31f6a745d172d7
Ankitha C <ankithac45@gmail.com>
2023-03-27 17:15:44 +0530

    Add files via upload

---
+++ 83. Remove Duplicates from Sorted List.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode temp=head;
+        while(temp.next!=null){
+            if(temp.val==temp.next.val){
+                ListNode skip=temp.next;
+                temp.next=skip.next;
+            }else{
+                temp=temp.next;
+            }
+        }
+        return head;
+    }
+}
