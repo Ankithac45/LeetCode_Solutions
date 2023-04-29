ff00a28cd865c2549b5cc0ce504ef0ffc8bde2b4
Ankitha C <ankithac45@gmail.com>
2023-04-29 19:22:28 +0530

    Add files via upload

---
+++ 412. Fizz Buzz.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> answer=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                answer.add("FizzBuzz");
+            }else if(i%3==0){
+                answer.add("Fizz");
+            }else if(i%5==0){
+                answer.add("Buzz");
+            }else{
+                answer.add(Integer.toString(i));
+            }
+        }
+        return answer;
+    }
+}
