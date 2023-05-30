4c991b338cd61a49f7d1611155ff63d89cef46a6
Ankitha C <ankithac45@gmail.com>
2023-05-30 12:40:53 +0530

    Add files via upload

---
+++ 705. Design HashSet.java
@@ -0,0 +1,20 @@
+class MyHashSet {
+    private final int ARRAY_SIZE = 1000001; 
+    private boolean[] buckets;
+
+    public MyHashSet() {
+        buckets = new boolean[ARRAY_SIZE];
+    }
+
+    public void add(int key) {
+        buckets[key] = true;
+    }
+
+    public void remove(int key) {
+        buckets[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return buckets[key];
+    }
+}
