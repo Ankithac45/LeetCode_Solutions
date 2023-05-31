b3f880721d9915bf702b4634dc1e6dead081ab7a
Ankitha C <ankithac45@gmail.com>
2023-05-31 15:58:44 +0530

    Add files via upload

---
+++ 75. Sort Colors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+
+        while(mid<=high){
+            if(nums[mid] == 0){
+                int temp = nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+               int temp = nums[mid];
+               nums[mid]= nums[high];
+               nums[high]=temp;
+               high--;
+            }
+        }
+    }
+}
