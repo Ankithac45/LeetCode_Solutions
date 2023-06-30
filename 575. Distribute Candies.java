d513e38125f4d166687f2efffb2ba18a8acb8e96
Ankitha C <ankithac45@gmail.com>
2023-06-30 11:46:09 +0530

    Add files via upload

---
+++ 575. Distribute Candies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        int n = candyType.length2;
+        SetInteger map = new HashSet();
+        for(int numcandyType){
+            map.add(num);
+        }
+        int count = map.size();
+        
+        if (count = n) {
+            return n;
+        } else {
+            return count;
+        }
+    }
+}
