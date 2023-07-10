511825868a026db3051507e6af9b19858794147b
Ankitha C <ankithac45@gmail.com>
2023-07-10 22:10:41 +0530

    Add files via upload

--- 136. Single Number.java
+++ 136. Single Number.java
@@ -1,21 +1,10 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int count=1;
-        Arrays.sort(nums);
-
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
-                count++;
-            }else{
-                if(count==1){
-                    return nums[i-1];
-                }
-                count=1;
-                if(i==nums.length-1 && count==1){
-                    return nums[i];
-                }
-            }
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(int num:nums){
+            result^=num;
         }
-        return nums[0];
+        return result;
     }
-}
+};
