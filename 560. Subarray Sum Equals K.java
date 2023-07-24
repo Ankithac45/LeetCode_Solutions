1b6e263f996e6e4a51c136f6d80a19c8b5021b5e
Ankitha C <ankithac45@gmail.com>
2023-07-24 11:38:25 +0530

    Add files via upload

---
+++ 560. Subarray Sum Equals K.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : nums) {
+            sum += num;
+            if(sum==k) count++;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        
+        return count;
+    }
+}
