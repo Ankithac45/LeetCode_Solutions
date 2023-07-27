943f2e2feb47d9b58d7ab38e131d7170ac9a732e
Ankitha C <ankithac45@gmail.com>
2023-07-27 09:07:10 +0530

    Add files via upload

---
+++ 81. Search in Rotated Sorted Array II.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[low]==nums[mid]&& nums[mid]==nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;   
+    }
+}
