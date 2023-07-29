96319c4536e8e20e2ebffefa270110946ba76bd1
Ankitha C <ankithac45@gmail.com>
2023-07-29 09:38:23 +0530

    Add files via upload

---
+++ 614. Maximum Nesting Depth of the Parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDepth(String s) {
+        int maxDepth = 0;
+        int currentDepth = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                currentDepth++;
+                maxDepth = Math.max(maxDepth, currentDepth);
+            } else if (c == ')') {
+                currentDepth--;
+            }
+        }
+
+        return maxDepth;
+    }
+}
