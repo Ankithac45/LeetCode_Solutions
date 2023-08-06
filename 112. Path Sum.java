1c3f933239109f210fa2f809830b69f1337588b0
Ankitha C <ankithac45@gmail.com>
2023-08-06 10:36:13 +0530

    Add files via upload

---
+++ 112. Path Sum.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+
+        // Check if the current node is a leaf node
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+
+        // Recursive calls for left and right subtrees
+        int remainingSum = targetSum - root.val;
+        return hasPathSum(root.left, remainingSum) || hasPathSum(root.right, remainingSum);
+    }
+}
