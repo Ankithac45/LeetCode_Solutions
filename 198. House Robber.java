9437ede91d97f9437d88a1a368080e5ed12a5e3c
Ankitha C <ankithac45@gmail.com>
2023-08-07 12:35:48 +0530

    Add files via upload

---
+++ 198. House Robber.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], (nums[i] + dp[i - 2]));
+        }
+        return dp[n - 1];
+
+        //Optimal Approach
+        // int n = nums.length;
+        // if (n == 0) return 0;
+        // if (n == 1) return nums[0];
+
+        // int prev2 = nums[0];
+        // int prev = Math.max(nums[0], nums[1]);
+        // int curr = prev;
+
+        // for (int i = 2; i < n; i++) {
+        //     curr = Math.max(prev, nums[i] + prev2);
+        //     prev2 = prev;
+        //     prev = curr;
+        // }
+        // return curr;
+    }
+}
