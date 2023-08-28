7d7f2005f5e2ea3f0cd63a722e1af5e03689dd35
Ankitha C <ankithac45@gmail.com>
2023-08-28 13:51:11 +0530

    Add files via upload

---
+++ 746. Min Cost Climbing Stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String toLowerCase(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'A' && chars[i] <= 'Z') {
+                chars[i] = (char)(chars[i] + ('a' - 'A')); /
+            }
+        }
+        return new String(chars);
+    }
+}
