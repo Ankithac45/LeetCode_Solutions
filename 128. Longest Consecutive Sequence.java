1473f926af146130af531b2c2500c43a3f6204e0
Ankitha C <ankithac45@gmail.com>
2023-09-09 23:08:50 +0530

    Add files via upload

---
+++ 128. Longest Consecutive Sequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) { 
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak); 
+            }
+        }
+
+        return longestStreak;
+    }
+}
